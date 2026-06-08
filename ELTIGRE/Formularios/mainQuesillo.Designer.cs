f5e438904a7ff34e6f0764a42c8c845c4028dc50
SebastianProgram76 <matias.duartef06@gmail.com>
2026-06-08 11:16:30 -0600

    Estoy intenttando que el github me reciba la actu pero a los otros no les sale para descargar

--- ELTIGRE/Formularios/mainQuesillo.Designer.cs
+++ ELTIGRE/Formularios/mainQuesillo.Designer.cs
@@ -82,43 +82,43 @@
             // facturaciónToolStripMenuItem
             // 
             this.facturaciónToolStripMenuItem.Name = "facturaciónToolStripMenuItem";
-            this.facturaciónToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.facturaciónToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.facturaciónToolStripMenuItem.Text = "Facturación";
             // 
             // inventarioToolStripMenuItem
             // 
             this.inventarioToolStripMenuItem.Name = "inventarioToolStripMenuItem";
-            this.inventarioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.inventarioToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.inventarioToolStripMenuItem.Text = "Inventario";
             // 
             // catalogoClienteToolStripMenuItem
             // 
             this.catalogoClienteToolStripMenuItem.Name = "catalogoClienteToolStripMenuItem";
-            this.catalogoClienteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.catalogoClienteToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.catalogoClienteToolStripMenuItem.Text = "Catalogo Cliente";
             // 
             // proveedoresToolStripMenuItem
             // 
             this.proveedoresToolStripMenuItem.Name = "proveedoresToolStripMenuItem";
-            this.proveedoresToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.proveedoresToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.proveedoresToolStripMenuItem.Text = "Proveedores";
             // 
             // sucursalesToolStripMenuItem
             // 
             this.sucursalesToolStripMenuItem.Name = "sucursalesToolStripMenuItem";
-            this.sucursalesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sucursalesToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.sucursalesToolStripMenuItem.Text = "Sucursales";
             // 
             // planillaToolStripMenuItem
             // 
             this.planillaToolStripMenuItem.Name = "planillaToolStripMenuItem";
-            this.planillaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.planillaToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.planillaToolStripMenuItem.Text = "Planilla";
             // 
             // contabilidadToolStripMenuItem
             // 
             this.contabilidadToolStripMenuItem.Name = "contabilidadToolStripMenuItem";
-            this.contabilidadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.contabilidadToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
             this.contabilidadToolStripMenuItem.Text = "Contabilidad";
             // 
             // mainQuesillo
@@ -131,6 +131,7 @@
             this.IsMdiContainer = true;
             this.Name = "mainQuesillo";
             this.Text = "Quesillos ELTIGRE";
+            this.Load += new System.EventHandler(this.mainQuesillo_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
--- ELTIGRE/Formularios/mainQuesillo.cs
+++ ELTIGRE/Formularios/mainQuesillo.cs
@@ -68,12 +68,12 @@ namespace ELTIGRE.Formularios
 
         private void ToolBarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            toolStrip.Visible = toolBarToolStripMenuItem.Checked;
+            //toolStrip.Visible = toolBarToolStripMenuItem.Checked;
         }
 
         private void StatusBarToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            statusStrip.Visible = statusBarToolStripMenuItem.Checked;
+            //statusStrip.Visible = statusBarToolStripMenuItem.Checked;
         }
 
         private void CascadeToolStripMenuItem_Click(object sender, EventArgs e)
@@ -102,6 +102,11 @@ namespace ELTIGRE.Formularios
             {
                 childForm.Close();
             }
+        }
+
+        private void mainQuesillo_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
