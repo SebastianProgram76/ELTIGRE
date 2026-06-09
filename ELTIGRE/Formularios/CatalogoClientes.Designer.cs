c207719151e9e1c9d2f3f02931298316b202da62
jcastilloma-png <j.castilloma@fia.unicit.edu.ni>
2026-06-09 08:34:41 -0600

    Actualizacion del formulario catalogo de cliente

--- ELTIGRE/Formularios/CatalogoClientes.Designer.cs
+++ ELTIGRE/Formularios/CatalogoClientes.Designer.cs
@@ -28,12 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BAgregarClientes = new System.Windows.Forms.Button();
+            this.BActualizarClientes = new System.Windows.Forms.Button();
+            this.BEliminarClientes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BAgregarClientes
+            // 
+            this.BAgregarClientes.Location = new System.Drawing.Point(418, 144);
+            this.BAgregarClientes.Name = "BAgregarClientes";
+            this.BAgregarClientes.Size = new System.Drawing.Size(75, 23);
+            this.BAgregarClientes.TabIndex = 0;
+            this.BAgregarClientes.Text = "Agregar";
+            this.BAgregarClientes.UseVisualStyleBackColor = true;
+            // 
+            // BActualizarClientes
+            // 
+            this.BActualizarClientes.Location = new System.Drawing.Point(418, 184);
+            this.BActualizarClientes.Name = "BActualizarClientes";
+            this.BActualizarClientes.Size = new System.Drawing.Size(75, 23);
+            this.BActualizarClientes.TabIndex = 1;
+            this.BActualizarClientes.Text = "Actualizar";
+            this.BActualizarClientes.UseVisualStyleBackColor = true;
+            // 
+            // BEliminarClientes
+            // 
+            this.BEliminarClientes.Location = new System.Drawing.Point(418, 230);
+            this.BEliminarClientes.Name = "BEliminarClientes";
+            this.BEliminarClientes.Size = new System.Drawing.Size(75, 23);
+            this.BEliminarClientes.TabIndex = 2;
+            this.BEliminarClientes.Text = "Eliminar";
+            this.BEliminarClientes.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(296, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Catalogo de Cliente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Apellido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(60, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefono:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Departamento:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(150, 136);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(150, 168);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(150, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(121, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(150, 232);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 86);
+            this.panel1.TabIndex = 12;
+            // 
+            // CatalogoClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BEliminarClientes);
+            this.Controls.Add(this.BActualizarClientes);
+            this.Controls.Add(this.BAgregarClientes);
+            this.Name = "CatalogoClientes";
             this.Text = "CatalogoClientes";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BAgregarClientes;
+        private System.Windows.Forms.Button BActualizarClientes;
+        private System.Windows.Forms.Button BEliminarClientes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
