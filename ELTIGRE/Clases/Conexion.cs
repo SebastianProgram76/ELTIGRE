87e7a559449539e21cc59efb942227dff4578f09
jcastilloma-png <j.castilloma@fia.unicit.edu.ni>
2026-06-09 09:26:52 -0600

    Incluyendo clases para la conexion de la base de datos

--- ELTIGRE/Clases/Conexion.cs
+++ ELTIGRE/Clases/Conexion.cs
@@ -3,11 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
 
 namespace ELTIGRE.Clases
 {
     internal class Conexion
     {
-
+        protected SqlConnection cxndb;
+        
+        protected bool Conectar()
+        {
+            try
+            {
+                cxndb = new SqlConnection(/*Aun incluir Properties.Settings.Default.cnn*/);
+                cxndb.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR Conexion: No se pudo establecer enlace con la db, llamar al mostro de sistema.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        protected bool Desconectar()
+        {
+            try
+            {
+                if(cxndb.State == ConnectionState.Open)
+                {
+                    cxndb.Close();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
     }
 }
